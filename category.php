3e463c12efefd32d9ce96642eaf74afa6ff8ac40
bpongy <bpongy@axome.com>
2013-10-21 10:11:47 +0200

    création template category.php

--- category.php
+++ category.php
@@ -1,8 +1,12 @@
+
 <?php get_template_part('templates/head'); ?>
 
 <div class="row">
 
 	<div id="main" class="col-sm-9" role="main">
+		
+		<h1><?php single_cat_title(); ?></h1>
+		
 		<?php if (!have_posts()) : ?>
 			<div class="alert">
 				<?php _e('Sorry, nothing found.', 'twoobl'); ?>
