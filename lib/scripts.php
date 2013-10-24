be009e75a22c7d49ecf744695b59a0af888c157b
bpongy <bpongy@axome.com>
2013-10-24 10:49:28 +0200

    dequeue jQuery migrate

--- lib/scripts.php
+++ lib/scripts.php
@@ -7,6 +7,19 @@
 
 
 
+
+
+/* dequeue jQuery migrate */
+function dequeue_jquery_migrate( &$scripts){
+	if(!is_admin()){
+		$scripts->remove('jquery');
+		$scripts->add('jquery', false, array('jquery-core'), '1.10.2');
+	}
+}
+add_filter( 'wp_default_scripts', 'dequeue_jquery_migrate' );
+
+
+
 function twoobl_scripts() {
 	wp_enqueue_style('twoobl_bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.css', false, null);
 	wp_enqueue_style('twoobl_main', get_template_directory_uri() . '/assets/css/main.css', false, null);
