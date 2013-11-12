9005414dcb4aeb9c35f18dd665818bf206c5585b
bpongy <bpongy@axome.com>
2013-11-12 15:26:30 +0100

    comments template on page.php

--- page.php
+++ page.php
@@ -13,10 +13,13 @@
 				<?php the_content(); ?>
 			</div>
 			<?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
+			<?php comments_template(); ?>
 		<?php endwhile; ?>
+
 	</div>
 
 	<?php get_template_part('templates/sidebar'); ?>
+
 </div>
 
 <?php get_template_part('templates/footer'); ?>
