ae3ef3ce4a09d893ca5ce0b95cc32a4cbdb6a488
bpongy <bpongy@axome.com>
2014-01-31 08:54:00 +0100

    html5shiv et respond via cdn

--- templates/head.php
+++ templates/head.php
@@ -10,8 +10,8 @@
 
 	<!-- IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-	<script src="<?php echo get_template_directory_uri(); ?>/assets/js/html5shiv.js"></script>
-	<script src="<?php echo get_template_directory_uri(); ?>/assets/js/respond.min.js"></script>
+	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 	<![endif]-->
 </head>
 
