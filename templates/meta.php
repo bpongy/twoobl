30acd2f8d4fd4fb62e245aa5ffbeae61686e801c
bpongy <bpongy@axome.com>
2014-03-06 09:40:05 +0100

    meta: mini amélioration

--- templates/meta.php
+++ templates/meta.php
@@ -1,5 +1,7 @@
 <div class="meta">
 	<span class="byline author vcard"><?php echo __('By', 'twoobl'); ?> <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?php echo get_the_author(); ?></a></span>
 	- <time itemprop="datePublished" content="<?php echo get_the_time('Y-m-d'); ?>" class="updated" datetime="<?php echo get_the_time('c'); ?>" pubdate><?php echo get_the_date(); ?></time>
+	<?php if ( comments_open() || !(!comments_open() && !have_comments()) ) { ?>
 	- <a href="<?php comments_link(); ?>"><?php comments_number( __('no response', 'twoobl'), __('one response', 'twoobl'), __('% responses', 'twoobl') ); ?></a>
+	<?php } ?>
 </div>
