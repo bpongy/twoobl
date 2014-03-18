6b46ee4bd311861b5b5fb86e8bcda622cea939dd
bpongy <bpongy@axome.com>
2014-03-18 11:24:48 +0100

    tests compass

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@
 
 http_path = "/"
 css_dir = "assets/css"
-sass_dir = "assets/css/bootstrap/sass"
+sass_dir = "assets/css/bootstrap"
 images_dir = "assets/img"
 javascripts_dir = "assets/js"
 
@@ -11,4 +11,6 @@ javascripts_dir = "assets/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :expanded
+output_style = :nested
+line_comments = false
+#environment = :development
