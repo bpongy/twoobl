6f71be2943fa4578a585ad997a2013136aef96a2
bpongy <bpongy@axome.com>
2014-03-18 10:22:24 +0100

    test SASS

---
+++ config.rb
@@ -0,0 +1,14 @@
+
+
+
+http_path = "/"
+css_dir = "assets/css"
+sass_dir = "assets/css/bootstrap/sass"
+images_dir = "assets/img"
+javascripts_dir = "assets/js"
+
+
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
