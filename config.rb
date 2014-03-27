c78e1106f606ed8b9caf7a7df57de5edbf1421f4
bpongy <bpongy@axome.com>
2014-03-27 16:28:40 +0100

    sass output type expanded, during tests

--- config.rb
+++ config.rb
@@ -11,6 +11,6 @@ javascripts_dir = "assets/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :compressed
+output_style = :expanded
 #line_comments = false
 #environment = :development
