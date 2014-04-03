6a10b2aa403ac2a31a02f70e08e3159eb9d45618
bpongy <bpongy@axome.com>
2014-04-03 10:27:18 +0200

    mobilenav, easier

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 /* constants */
 define('_MAIN_CLASS_', 'col-sm-9');
 define('_SIDEBAR_CLASS_', 'col-sm-3 hidden-xs');
+define('_MOBILE_NAV_', true);
 define('_ISOLANG_', substr(get_bloginfo('language'), 0, 2));
 
 $twoobl = pods('twoobl');
@@ -12,5 +13,4 @@ require_once locate_template('/lib/widgets.php');
 require_once locate_template('/lib/shortcodes.php');
 require_once locate_template('/lib/pods-init.php');
 require_once locate_template('/lib/twoobl.php');
-require_once locate_template('/lib/mobilenav.php');
 require_once locate_template('/lib/custom.php');
