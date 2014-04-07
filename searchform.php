b87e4d541a5ab471978d64b562b9de60951b894e
bpongy <bpongy@axome.com>
2014-04-07 14:01:46 +0200

    new searchform template

--- searchform.php
+++ searchform.php
@@ -1,5 +1,9 @@
-<form role="search" class="form-inline" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
 	<label class="hidden" for="s">Search for:</label>
-	<div class="form-group"><input type="text" class="form-control" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" id="s" placeholder="<?php _e('Search', 'twoobl'); ?>" /></div>
-	<button type="submit" id="searchsubmit" class="btn btn-default"><?php _e('OK', 'twoobl'); ?></button>
+	<div class="input-group">
+		<input type="text" class="form-control" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" id="s" placeholder="<?php _e('Search', 'twoobl'); ?>" />
+		<span class="input-group-btn">
+			<button type="submit" id="searchsubmit" class="btn btn-default"><?php _e('OK', 'twoobl'); ?></button>
+		</span>
+	</div>
 </form>
