5bcaf470c68340b77ab43399745855eca99d28b9
bpongy <bpongy@axome.com>
2014-04-16 11:42:51 +0200

    grunt :)

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+'use strict';
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg : grunt.file.readJSON('package.json'),
+		concat : {
+			options : {
+				separator : ';'
+			},
+			bootstrap : {
+				src : [
+					'assets/js/bootstrap/affix.js',
+					'assets/js/bootstrap/alert.js',
+					'assets/js/bootstrap/button.js',
+					'assets/js/bootstrap/carousel.js',
+					'assets/js/bootstrap/collapse.js',
+					'assets/js/bootstrap/dropdown.js',
+					'assets/js/bootstrap/modal.js',
+					'assets/js/bootstrap/popover.js',
+					'assets/js/bootstrap/scrollspy.js',
+					'assets/js/bootstrap/tab.js',
+					'assets/js/bootstrap/tooltip.js',
+					'assets/js/bootstrap/transitions.js',
+				],
+				dest : 'assets/js/bootstrap.js'
+			}
+		},
+		uglify : {
+			twoobl_main : {
+				options : {
+					banner : '/*! <%= pkg.name %> - <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+					mangle: false
+				},
+				files: {
+					'assets/js/main.min.js' : ['<%= concat.bootstrap.dest %>', 'assets/js/plugins.js', 'assets/js/main.js']
+				}
+			}
+		},
+		compass: {
+			compile: {
+				options: {
+					config: 'config.rb'
+				}
+			}
+		},
+		watch : {
+			css: {
+				files: 'assets/scss/*.scss',
+				tasks: ['compass']
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['concat', 'uglify', 'watch']);
+
+}; 
--- config.rb
+++ config.rb
@@ -12,5 +12,5 @@ javascripts_dir = "assets/js"
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 output_style = :expanded
-#line_comments = false
+line_comments = false
 #environment = :development
