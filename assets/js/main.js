5192b6e87820df1cab430982473b059dfab50061
Sylvain Gougouzian <gouz@axome.com>
2014-05-13 08:52:17 +0200

    [+] comportement au click pour fermer la mobile nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,14 @@
 		
 		// Bootstrap tooltips
 		$('a[data-toggle="tooltip"]').tooltip();
+		
+		// mobilenav
+		var mobile_nav_opened = false;
+		$(document).on('click', function(event) {
+			if (event.target.id != 'menu-button' && $('#check-navmob').is(':checked') && mobile_nav_opened)
+				$('#check-navmob').prop('checked', '');
+			mobile_nav_opened = $('#check-navmob').is(':checked');
+		});
 
 	});
 })(window.jQuery);
