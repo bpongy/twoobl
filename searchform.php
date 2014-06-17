ab7539ca7dbf0f5db62d68b500841fb8ff626e41
bpongy <bpongy@axome.com>
2014-06-17 15:15:23 +0200

    searchform: sr-only label

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-	<label class="hidden" for="s">Search for:</label>
+	<label class="sr-only" for="s">Search for:</label>
 	<div class="input-group">
 		<input type="text" class="form-control" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" id="s" placeholder="<?php _e('Search', 'twoobl'); ?>" />
 		<span class="input-group-btn">
