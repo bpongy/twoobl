9a01ef08769dd0ddbabb46bb4046402d4052615d
bpongy <bpongy@axome.com>
2015-01-08 10:06:28 +0100

    article thumbnail

--- templates/content.php
+++ templates/content.php
@@ -3,6 +3,13 @@
 		<h2 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 		<?php get_template_part('templates/meta'); ?>
 	</header>
+	<?php
+	if ( has_post_thumbnail() ) {
+		echo '<a class="thumb" href="'.get_the_permalink().'">';
+		the_post_thumbnail('large');
+		echo '</a>';
+	}
+	?>
 	<div class="entry-content clearfix">
 		<?php the_excerpt(); ?>
 	</div>
