70953ddb58d1bf8cee43a035567e494c6ed89da4
Benjamin PONGY <bpongy@axome.com>
2015-04-01 09:51:32 +0200

    BS3 update

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 /* constants */
 define('_MAIN_CLASS_', 'col-sm-9');
 define('_SIDEBAR_CLASS_', 'col-sm-3 hidden-xs');
+define('_THEME_COLOR_', '#444');
 define('_MOBILE_NAV_', true);
 define('_THUMBNAIL_COLUMN_', true);
 define('_TWOOBL_DEV_', WP_DEBUG);
