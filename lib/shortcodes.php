16656f397d4b8ebbce08384745ffb46a3f41f83c
Benjamin PONGY <benjamin.pongy@gmail.com>
2015-07-22 11:33:47 +0200

    shortcode example: gist integration

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -13,6 +13,18 @@ add_shortcode('twooblexample', 'shortcode_twooblexample');
 
 
 
+// another shortcode example: gist integration
+function twoobl_gist($atts){
+	extract(shortcode_atts(array(
+		'id' => false
+	), $atts));
+
+	return '<div class="gist"><script src="https://gist.github.com/'.$id.'.js"></script></div>';
+}
+add_shortcode('gist', 'twoobl_gist');
+
+
+
 /******************************************************************
  * 	remove <p> and <br /> around shortcodes
  * 	you don't need it with Bootstrap Shortcodes plugins.
