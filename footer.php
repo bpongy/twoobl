29ba72ed73de43554e993414da3b8afaa9a406b7
Benjamin PONGY <benjamin.pongy@gmail.com>
2015-07-22 11:32:45 +0200

    debug info for admins only

--- footer.php
+++ footer.php
@@ -20,7 +20,7 @@ if( defined( '_MOBILE_NAV_' ) && _MOBILE_NAV_ ) {
 <?php wp_footer(); ?>
 
 <?php
-if( defined('_TWOOBL_DEV_') && _TWOOBL_DEV_ ) {
+if( defined('_TWOOBL_DEV_') && _TWOOBL_DEV_ && current_user_can('activate_plugins') ) {
 	echo '<div class="debug-twoobl debug-bottom">';
 		echo '<span class="visible-xs-inline label label-danger">xs</span>';
 		echo '<span class="visible-sm-inline label label-danger">sm</span>';
