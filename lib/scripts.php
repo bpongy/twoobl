988d9bcb0d9c121208bca601784f23a520de18b0
Benjamin PONGY <benjamin.pongy@gmail.com>
2015-08-12 11:18:36 +0200

    jquery 1.11.3

--- lib/scripts.php
+++ lib/scripts.php
@@ -14,7 +14,7 @@ if( !function_exists( 'twoobl_scripts' ) ) {
 		if (!is_admin()) {
 			// Be careful, this method will dequeue jQuery migrate
 			wp_deregister_script('jquery');
-			wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js', false, null, false);
+			wp_register_script('jquery', 'https://code.jquery.com/jquery-1.11.3.min.js', false, null, false);
 			add_filter('script_loader_src', 'twoobl_jquery_local_fallback', 10, 2);
 		}
 
@@ -40,7 +40,7 @@ if( !function_exists( 'twoobl_jquery_local_fallback' ) ) {
 		static $add_jquery_fallback = false;
 	
 		if ($add_jquery_fallback) {
-			echo '<script>window.jQuery || document.write(\'<script src="' . get_template_directory_uri() . '/assets/js/jquery-1.10.2.min.js"><\/script>\')</script>' . "\n";
+			echo '<script>window.jQuery || document.write(\'<script src="' . get_template_directory_uri() . '/assets/js/jquery-1.11.3.min.js"><\/script>\')</script>' . "\n";
 			$add_jquery_fallback = false;
 		}
 	
