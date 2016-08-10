a8e04a466b9f3705f3a9a0534d0cdc37e9f9d3ef
Benjamin PONGY <benjamin.pongy@gmail.com>
2016-08-10 15:51:54 +0200

    Update page-large.php

--- page-large.php
+++ page-large.php
@@ -2,37 +2,21 @@
 /*
 Template Name: Large page
 */
-
-
-
-
-
-/* TODO */
-
-
-
-
 ?>
 
 <?php get_header(); ?>
 
-<div class="row">
-	
-	<div id="main" class="col-xs-12" role="main">
-	
-		<div class="entry-header">
-		  <h1 class="title"><?php the_title(); ?></h1>
-		</div>
-	
-		<?php while (have_posts()) : the_post(); ?>
-			<div class="entry-content clearfix">
-				<?php the_content(); ?>
-			</div>
-			<?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
-			<?php comments_template(); ?>
-		<?php endwhile; ?>
+<div id="main" role="main">
+	<div class="entry-header">
+	  <h1 class="title"><?php the_title(); ?></h1>
 	</div>
-
+	<?php while (have_posts()) : the_post(); ?>
+		<div class="entry-content clearfix">
+			<?php the_content(); ?>
+		</div>
+		<?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
+		<?php comments_template(); ?>
+	<?php endwhile; ?>
 </div>
 
 <?php get_footer(); ?>
