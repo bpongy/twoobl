a3c07089a9db33f2a13c0e6de74901e0940fead2
redpik <benjamin.pongy@gmail.com>
2017-01-24 09:29:55 +0100

    [*] jquery version defined in functions.php file

--- functions.php
+++ functions.php
@@ -5,15 +5,15 @@ define('_MAIN_CLASS_', 'col-sm-9 col-sm-push-3');
 define('_SIDEBAR_CLASS_', 'col-sm-3 col-sm-pull-9 hidden-xs');
 define('_THEME_COLOR_', '#444');
 define('_MOBILE_NAV_', true);
-define('_THUMBNAIL_COLUMN_', true);
 define('_TWOOBL_DEV_', WP_DEBUG);
 define('_TWOOBL_SCR_VERSION_', '2.1');
+define('_JQUERY_VERSION_', '3.1.1');	// 1.12.4, 2.2.4 or 3.1.1
 define('_ISOLANG_', substr(get_bloginfo('language'), 0, 2));
 
 require_once locate_template('lib/scripts.php');
 require_once locate_template('lib/widgets.php');
 require_once locate_template('lib/shortcodes.php');
-require_once locate_template('lib/acf-init.php');
+//require_once locate_template('lib/acf-init.php');
 require_once locate_template('lib/tinymce.php');
 require_once locate_template('lib/siteaccesschecker.php');
 require_once locate_template('lib/twoobl.php');
