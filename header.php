2e48ac31b5a9e9e6dda3d5ce4876f75347218e9d
redpik <benjamin.pongy@gmail.com>
2017-05-17 08:53:33 +0200

    [-] drop X-UA-Compatible

--- header.php
+++ header.php
@@ -2,8 +2,8 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
 	<!-- IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
