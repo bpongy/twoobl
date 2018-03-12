adfa00f1675869c668b39f0fb9fdc56c23d55a4a
redpik <benjamin.pongy@gmail.com>
2018-03-12 17:35:38 +0100

    [-] remove microdata

--- templates/meta.php
+++ templates/meta.php
@@ -1,6 +1,6 @@
 <div class="meta">
-	<span class="byline author vcard"><?php echo __('By', 'twoobl'); ?> <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn p-author h-card"><?php echo get_the_author(); ?></a></span>
-	- <time content="<?php echo get_the_time('Y-m-d'); ?>" class="updated dt-published" datetime="<?php echo get_the_time('c'); ?>" pubdate><?php echo get_the_date(); ?></time>
+	<span class="author"><?php echo __('By', 'twoobl'); ?> <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php echo get_the_author(); ?></a></span>
+	- <time content="<?php echo get_the_time('Y-m-d'); ?>" class="updated" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
 	<?php if ( comments_open() || !(!comments_open() && !have_comments()) ) { ?>
 	- <a href="<?php comments_link(); ?>"><?php comments_number( __('no response', 'twoobl'), __('one response', 'twoobl'), __('% responses', 'twoobl') ); ?></a>
 	<?php } ?>
