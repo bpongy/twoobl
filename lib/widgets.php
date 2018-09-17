bc4651c2c3640ca9486664d7eeee0206cadff981
redpik <benjamin.pongy@gmail.com>
2018-09-17 15:31:06 +0200

    No more Footer and Mobile Menu widgets zones

--- lib/widgets.php
+++ lib/widgets.php
@@ -16,25 +16,6 @@ if( !function_exists( 'twoobl_widgets_init' ) ) {
 			'after_widget'  => '</div>'
 		));
 	
-		register_sidebar(array(
-			'name'          => __('Footer', 'twoobl'),
-			'id'            => 'footer',
-			'before_widget' => '<div id="%1$s" class="widget %2$s clearfix">',
-			'before_title'  => '<span class="title">',
-			'after_title'   => '</span>',
-			'after_widget'  => '</div>'
-		));
-	
-		if( defined( '_MOBILE_NAV_' ) && _MOBILE_NAV_ )
-			register_sidebar(array(
-				'name'          => __('Mobile menu', 'twoobl'),
-				'id'            => 'mobile_menu',
-				'before_widget' => '<div id="%1$s" class="widget %2$s clearfix">',
-				'before_title'  => '<span class="title">',
-				'after_title'   => '</span>',
-				'after_widget'  => '</div>'
-			));
-	
 		//on suppr les widgets par défaut qui servent à rien
 		unregister_widget('WP_Widget_Meta');
 		unregister_widget('WP_Widget_Tag_Cloud');
