42c7ce17995ee8f05dff44aecb9b5d327fa26e26
Benjamin PONGY <bpongy@axome.com>
2019-01-15 10:46:13 +0100

    [+] small cleanings

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ define('_SIDEBAR_CLASS_', 'col order-1 d-none d-md-block');
 define('_THEME_COLOR_', '#444');
 define('_MOBILE_NAV_', true);
 define('_TWOOBL_DEV_', true);
-define('_JQUERY_VERSION_', '2.2.4');	// 2.2.4 or 3.3.1
+define('_JQUERY_VERSION_', '2.2.4');
 define('_ISOLANG_', substr(get_bloginfo('language'), 0, 2));
 
 require_once locate_template('lib/elementor.php');
