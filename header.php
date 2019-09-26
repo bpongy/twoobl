4a724cf7180510c67859e5d43d8fbb6baffdbd95
Benjamin PONGY <bpongy@axome.com>
2019-09-26 14:06:15 +0200

    [+] wp_body_open

--- header.php
+++ header.php
@@ -10,6 +10,12 @@
 <body <?php body_class(); ?>>
 
 <?php
+if ( function_exists( 'wp_body_open' ) ) {
+	wp_body_open();
+} else {
+	do_action( 'wp_body_open' );
+}
+
 if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'header' ) ) {
 	get_template_part( 'template-parts/header' );
 }
