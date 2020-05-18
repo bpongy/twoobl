ca5fee1e9721283229e054912295c1f37afdd78e
Benjamin PONGY <benjamin.pongy@gmail.com>
2020-05-18 21:13:48 +0200

    Update functions.php
    _MOBILE_NAV_ is unused.

--- functions.php
+++ functions.php
@@ -3,7 +3,6 @@ if ( ! defined( 'ABSPATH' ) ) { exit; }
 
 /* constants */
 define('_THEME_COLOR_', '#444');
-define('_MOBILE_NAV_', true);
 define('_ISOLANG_', substr(get_bloginfo('language'), 0, 2));
 
 require get_stylesheet_directory() . '/lib/elementor.php';
